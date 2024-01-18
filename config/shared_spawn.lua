a4ee1fc5a07b3640bcd04e6557cf27d452d542c2
PHIDIAS <phidias@aperx.org>
2024-01-18 23:26:02 +0900

    .

--- config/shared_spawn.lua
+++ config/shared_spawn.lua
@@ -2,8 +2,8 @@
 -- @config shared spawn
 
 return {
-    deconstruction_radius = 96,
-    base_radius = 88,
+    deconstruction_radius = 80,
+    base_radius = 72,
     deconstruction_tile = 'water',
     base_tile = 'landfill',
     resource_tiles = {
@@ -14,40 +14,35 @@ return {
                 name = 'iron-ore',
                 amount = 5000,
                 size = {26, 27},
-                offset = {-64,-32}
-                -- offset = {-64,-64}
+                offset = {-48,-32}
             },
             {
                 enabled = true,
                 name = 'copper-ore',
                 amount = 5000,
                 size = {26, 27},
-                offset = {-64, 0}
-                -- offset = {64, -64}
+                offset = {-48, 0}
             },
             {
                 enabled = true,
                 name = 'stone',
                 amount = 5000,
                 size = {26, 27},
-                offset = {-64, 32}
-                -- offset = {-64, 64}
+                offset = {-48, 32}
             },
             {
                 enabled = true,
                 name = 'coal',
                 amount = 5000,
                 size = {26, 27},
-                offset = {-64, -64}
-                -- offset = {64, 64}
+                offset = {-48, -64}
             },
             {
                 enabled = false,
                 name = 'uranium-ore',
                 amount = 5000,
                 size = {26, 27},
-                offset = {-64, -96}
-                -- offset = {0, 64}
+                offset = {-48, -96}
             }
         }
     },
@@ -59,10 +54,8 @@ return {
                 name = 'crude-oil',
                 num_patches = 4,
                 amount = 5000000,
-                offset = {-80, -12},
-                -- offset = {-12, 64},
+                offset = {-64, -12},
                 offset_next = {0, 6}
-                -- offset_next = {6, 0}
             }
         }
     },
