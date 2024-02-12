626a6736a0edfaa285b0d2b3e5d234c60e820153
PHIDIAS <phidias@aperx.org>
2024-02-12 15:11:06 +0900

    .

--- modules/commands/vlayer.lua
+++ modules/commands/vlayer.lua
@@ -14,7 +14,7 @@ Commands.new_command('personal-battery-recharge', 'Recharge Player Battery upto 
         if armor.equipment[i].energy < (armor.equipment[i].max_energy * amount) then
             local energy_required = math.min((armor.equipment[i].max_energy * amount) - armor.equipment[i].energy, vlayer.get_statistics()['energy_storage'])
             armor.equipment[i].energy = armor.equipment[i].max_energy + energy_required
-            vlayer.energy_changed(-energy_required)
+            vlayer.energy_changed(0 - energy_required)
         end
     end
 
